34f3a291a718a5736d729f7f8c2dbcc2a09f07b1
Tyler11234 <40501845+Tyler11234@users.noreply.github.com>
2018-06-22 14:17:47 -0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,64 @@
+import sys
+import math
+
+def pythagoras(a,b):
+    a_squared = math.pow(a,2)
+    b_squared = math.pow(b,2)
+    return math.sqrt(a_squared + b_squared)
+
+def main():
+    command = sys.argv[1]
+
+    if command == "add":
+        x = int(sys.argv[2])
+        y = int(sys.argv[3])
+        print(x + y)
+
+    if command == "sub":
+        x = int(sys.argv[2])
+        y = int(sys.argv[3])
+        print(x - y)
+
+    if command == "mul":
+        x = int(sys.argv[2])
+        y = int(sys.argv[3])
+        print(x * y)
+
+    if command == "div":
+        x = int(sys.argv[2])
+        y = int(sys.argv[3])
+        print(x / y)
+
+    if command == "exp":
+        x = int(sys.argv[2])
+        y = int(sys.argv[3])
+        print(x ** y)
+
+    if command == "countto":
+        x = int(sys.argv[2])
+        for i in range(x):
+            print(i)
+
+    if command == "hypot":
+        a = int(sys.argv[2])
+        b = int(sys.argv[3])
+
+        print(pythagoras(a, b))
+
+    if command == "sqrt":
+        x = int(sys.argv[2])
+        y = 0.5
+        print(x ** y)
+
+    if command == "curt":
+        x = int(sys.argv[2])
+        y = 0.3333333333333
+        print(x ** y)
+
+
+
+
+
+
+
+main()
---
+++ hello.py
@@ -0,0 +1,3 @@
+def main():
+    print("Hello, Tyler")
+main()
